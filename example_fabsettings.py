088bc5ee96eb08346210c83c11368a03961a7e01
Adrian Nye <adrian_nye@yahoo.com>
2015-01-22 18:32:14 -0600

    wip

---
+++ example_fabsettings.py
@@ -0,0 +1,9 @@
+# your projects settings
+
+WF_HOST       =     "adriannye.webfaction.com"
+PROJECT_NAME  =     "songrhyme"
+REPOSITORY    =     "git@github.com:adriannye/python_rhyme.git"
+USER          =     "adriannye"
+PASSWORD      =     "secret"
+SETTINGS_SUBDIR =   "songrhyme"
+VIRTUALENVS   =     "/home/adriannye/Virtualenvs"

e28e1ae116c8c97928b3fce38012ee85a9b13055
Adrian Nye <adrian_nye@yahoo.com>
2015-01-23 00:53:59 +0000

    deploy changes

--- songrhyme/project/wsgi.py
+++ songrhyme/project/wsgi.py
@@ -10,7 +10,7 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "songrhyme.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "songrhyme.project.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = Cling(get_wsgi_application())
